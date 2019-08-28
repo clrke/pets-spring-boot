f0efddedeb54aa75c45fc51587d531bddfd274ac
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2019-08-28 18:15:48 +0800

    Initial commit

---
+++ src/main/kotlin/com/whitecloak/pets/PetsApplication.kt
@@ -0,0 +1,11 @@
+package com.whitecloak.pets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetsApplication>(*args)
+}
